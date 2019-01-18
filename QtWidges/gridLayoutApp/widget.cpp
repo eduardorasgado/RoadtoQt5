8edb1756d5e3bdf94eef9a28e14e477039ba1c31
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-17 22:18:11 -0600

    expanding widgets in grid

--- QtWidges/gridLayoutApp/widget.cpp
+++ QtWidges/gridLayoutApp/widget.cpp
@@ -13,16 +13,17 @@ Widget::Widget(QWidget *parent) :
     // widget, position(row, column)
     layout->addWidget(ui->button1, 0, 0);
     layout->addWidget(ui->button2, 0, 1);
-    layout->addWidget(ui->button3, 0, 2);
+    // Expanding the button 2 rows and 1 column
+    layout->addWidget(ui->button3, 0, 2, 2, 1);
 
     // Second row
     layout->addWidget(ui->button4, 1, 0);
     layout->addWidget(ui->button5, 1, 1);
-    layout->addWidget(ui->button6, 1, 2);
+    //layout->addWidget(ui->button6, 1, 2);
 
     // thirth layout
-    layout->addWidget(ui->button7, 2, 0);
-    layout->addWidget(ui->button8, 2, 1);
+    layout->addWidget(ui->button7, 2, 0, 1, 2);
+    //layout->addWidget(ui->button8, 2, 1);
     layout->addWidget(ui->button9, 2, 2);
 
 }
