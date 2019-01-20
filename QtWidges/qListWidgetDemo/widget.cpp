8fc94f306c70e1577ddbb233279288be8c6ed279
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-20 17:59:36 -0600

    assignment form and logic to set vbuttons and form visibility

--- QtWidges/qListWidgetDemo/widget.cpp
+++ QtWidges/qListWidgetDemo/widget.cpp
@@ -16,16 +16,19 @@ Widget::~Widget()
 
 void Widget::on_button_add_assignment_clicked()
 {
-    //
-
+    // if add button then show a form
+    ui->box_asg_form->setVisible(true);
 }
 
 void Widget::initUI()
 {
-    // hide buttons if no elements in list
+    //no elements in list
     if(ui->list_assignments->count() == 0)
     {
+        // hide buttons
         ui->button_delete_assignment->setVisible(false);
         ui->button_selected_assignment->setVisible(false);
+        // hide form
+        ui->box_asg_form->setVisible(false);
     }
 }
