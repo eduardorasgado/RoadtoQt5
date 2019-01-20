dbba924ce491e7feb53bcde53686a52b6e3ee554
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-19 21:56:37 -0600

    combo box app: gui

---
+++ QtWidges/qComboBoxApp/main.cpp
@@ -0,0 +1,13 @@
+#include "widget.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    qputenv("QT_STYLE_OVERRIDE", "");
+    QApplication a(argc, argv);
+    Widget w;
+    w.setWindowTitle("Combo Box App");
+    w.show();
+
+    return a.exec();
+}
