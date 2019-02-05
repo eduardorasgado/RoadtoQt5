b4da28c3e724f28438764be32997d5ca873cf6cc
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-02-05 00:16:55 -0600

    changing button colors using color dialog, saving into a list all actual colors

--- qSettings/qSettingsDemo/widget.cpp
+++ qSettings/qSettingsDemo/widget.cpp
@@ -6,56 +6,69 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    initButtonList();
+    setDefaultColors();
 }
 
 Widget::~Widget()
 {
+    delete buttonList;
     delete ui;
 }
 
 void Widget::on_pushButton1_clicked()
 {
-
+    auto position = 0;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton2_clicked()
 {
-
+    auto position = 1;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton3_clicked()
 {
-
+    auto position = 2;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton4_clicked()
 {
-
+    auto position = 3;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton5_clicked()
 {
-
+    auto position = 4;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton6_clicked()
 {
-
+    auto position = 5;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton7_clicked()
 {
-
+    auto position = 6;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton8_clicked()
 {
-
+    auto position = 7;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton9_clicked()
 {
-
+    auto position = 8;
+    getNewColor(position);
 }
 
 void Widget::on_pushButton_saveColors_clicked()
@@ -66,4 +79,54 @@ void Widget::on_pushButton_saveColors_clicked()
 void Widget::on_pushButton_loadColors_clicked()
 {
 
+}
+
+void Widget::setDefaultColors()
+{
+    for(auto i : {1,2,3,4,5,6,7,8,9}){
+        // fill list with buttons number of elements and black color
+        // as value
+        colorList.push_back(Qt::black);
+    }
+
+    for(auto &c : colorList){
+        qDebug() << c;
+    }
+
+}
+
+void Widget::initButtonList()
+{
+    // allocating all buttons in a dynamic list
+    // to enhace button handling
+    buttonList = new QList<QWidget*>();
+
+    buttonList->push_back(ui->pushButton1);
+    buttonList->push_back(ui->pushButton2);
+    buttonList->push_back(ui->pushButton3);
+    buttonList->push_back(ui->pushButton4);
+    buttonList->push_back(ui->pushButton5);
+    buttonList->push_back(ui->pushButton6);
+    buttonList->push_back(ui->pushButton7);
+    buttonList->push_back(ui->pushButton8);
+    buttonList->push_back(ui->pushButton9);
+}
+
+void Widget::getNewColor(int &position)
+{
+    // getting new color
+    QColor color = QColorDialog::getColor(colorList[position],
+            this, "Choose Background Color");
+    if(color.isValid()){
+        // assigning new color to list
+        colorList[position] = color;
+
+        // show color to button
+        // deffining a stylesheet property
+        QString css = QString("background-color: %1")
+                // .name return name in RGB format
+                .arg(color.name());
+
+        buttonList->at(position)->setStyleSheet(css);
+    }
 }
--- qSettings/qSettingsDemo/widget.h
+++ qSettings/qSettingsDemo/widget.h
@@ -2,6 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QList>
+#include <QColor>
+#include <QButtonGroup>
+#include <QDebug>
+#include <QColorDialog>
 
 namespace Ui {
 class Widget;
@@ -15,8 +20,38 @@ public:
     explicit Widget(QWidget *parent = nullptr);
     ~Widget();
 
+private slots:
+    void on_pushButton1_clicked();
+
+    void on_pushButton2_clicked();
+
+    void on_pushButton3_clicked();
+
+    void on_pushButton4_clicked();
+
+    void on_pushButton5_clicked();
+
+    void on_pushButton6_clicked();
+
+    void on_pushButton7_clicked();
+
+    void on_pushButton8_clicked();
+
+    void on_pushButton9_clicked();
+
+    void on_pushButton_saveColors_clicked();
+
+    void on_pushButton_loadColors_clicked();
+
 private:
     Ui::Widget *ui;
+    // to store initial button colors
+    QList<QColor> colorList;
+    QList<QWidget*> *buttonList;
+
+    void setDefaultColors();
+    void initButtonList();
+    void getNewColor(int&);
 };
 
 #endif // WIDGET_H
